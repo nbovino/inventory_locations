1b5f0e067e106762936d6be38013fa5c99faa559
Nate Bovino <nbovino05@gmail.com>
2021-05-08 10:19:09 -0400

    Moved classes to main to help with possibly creating all new variables

--- main_testing.py
+++ main_testing.py
@@ -1,16 +1,41 @@
-# I need to make an object here that will be able to be drawn on the canvas make a computer class
-# and it will be a green circle that you an move around but only if you click where the circle is
 import tkinter as tk
+import classes
 from global_variables import *
 # from setup import *
+import pickle
 from PIL import ImageTk, Image
-# from setup import IMAGE_PATH
-# from main import canvas
+import os.path
+from os import path
+
+all_devices = []
+#
+IMAGE_PATH = "images/"
+LAYOUTS_PATH = "layouts/"
+DEVICE_ICONS_PATH = "device_icons/"
+#
+w = 600
+h = 400
+# # Starting coordinates of the circle
+x = w // 2
+y = h // 2
+
+root = tk.Tk()
+root.title('Inventory Locations')
+# root.iconbitmap("\\images\\greeen_circle.png")
+root.geometry("800x600")
+canvas = tk.Canvas(root, width=w, height=h, bg="white")
+# self.canvas.pack(pady=20)
+canvas.grid(row=1, rowspan=5, column=1, columnspan=8)
+# self.alert_message = tk.Label(self.root, text="HELLO")
+# Pack
+# self.alert_message.pack(pady=30, side=tk.RIGHT)
+# self.alert_message.grid(row=10, rowspan=1, column=1, columnspan=1)
 
 
 class DeviceIcon(object):
+    global root
     def __init__(self, canvas, image_name, xpos, ypos, root, device_name):
-        self.root = root
+        # self.root = root
         self.canvas = canvas
         self.image_name = image_name
         self.device_name = device_name
@@ -48,8 +73,8 @@ class DeviceIcon(object):
         self.move_flag = False
 
     def clicked(self, event):
-        self.root.selected_device = self
-        print(self.root.selected_device)
+        root.selected_device = self
+        print(root.selected_device)
         print("Device Selected - X: " + str(self.xpos) + " Y: " + str(self.ypos))
         print(self.device_name)
 
@@ -135,16 +160,54 @@ class MessageLabel(object):
         print("GOT HERE")
 
 
-# TODO: make a delete device button
-# class DeleteDeviceButton(object):
-#     def __init__(self, canvas):
-#         self.delete_device_button = tk.Button(self.root, text="Delete Device", command=self.delete_device)
-#         self.delete_device_button.pack(padx=60)
-#
-#         print("CREATED BUTTON")
-#
-#     def delete_device(self):
-#         if program_canvas.selected_device:
-#             print("Will delete device " + str(program_canvas.selected_device))
-#         else:
-#             print("No device selected")
+def save_devices():
+    # Can't pickle tKinter objects. Need to convert xpos, ypos, and image to dict
+    devices_to_pickle = []
+    for d in all_devices:
+        this_device = {'image_path': d.__dict__['image_path'],
+                       'xpos': d.__dict__['xpos'],
+                       'ypos': d.__dict__['ypos'],
+                       'device_name': d.__dict__['device_name']}
+        # devices_to_pickle.append(d.__dict__)
+        devices_to_pickle.append(this_device)
+    print(devices_to_pickle)
+    pickle_out = open("saved_locations\devices.pk1", "wb")
+    pickle.dump(devices_to_pickle, pickle_out)
+    pickle_out.close()
+    print("SAVED DEVICES!!!!")
+
+
+def add_device():
+    print("New device code to add")
+
+
+selected_device = None
+save_button = tk.Button(root, text="Save", command=save_devices)
+# self.save_button.pack(pady=20)
+save_button.grid(row=3, rowspan=1, column=10, columnspan=1)
+new_device_button = classes.NewDeviceButton(canvas, root, all_devices)
+# classes.DeleteDeviceButton(program_canvas.canvas)
+
+# Load floorplan
+if path.exists("saved_locations/devices.pk1"):
+    classes.FloorPlan(canvas, "apartment.png")
+else:
+    classes.FloorPlan(canvas, "apartment.png")
+
+# Load devices
+if path.exists("saved_locations/devices.pk1"):
+    pickle_in = open("saved_locations/devices.pk1", "rb")
+    saved_devices = pickle.load(pickle_in)
+    for d in saved_devices:
+        all_devices.append(classes.DeviceIcon(canvas,
+                                              d['image_path'].split("/")[-1:][0],
+                                              d['xpos'],
+                                              d['ypos'],
+                                              root,
+                                              d['device_name']))
+else:
+    print("No Devices on the floorplan")
+    # self.alert_message.config(text="No devices on this floorplan")
+
+
+root.mainloop()
